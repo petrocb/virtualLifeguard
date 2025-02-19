8cd297e807c71dd43f4d0a1e02d698917d1e38cf
petrocb <k2124745@kingston.ac.uk>
2025-02-19 12:07:04 +0000

    added some basic taking

---
+++ tkinterFreshStart/alerts/always.py
@@ -0,0 +1,11 @@
+class Moving:
+    def __init__(self):
+        self.x = 0
+
+    def step(self):
+        #create a better alter here!!!!!!!!!!!!!!!!!!!!!!!!
+        self.x += 1
+        if self.x == 100:
+            return True
+
+        return False
--- tkinterFreshStart/alerts/moving.py
+++
@@ -1,6 +0,0 @@
-class Moving:
-    def __init__(self):
-        pass
-
-    def test(self):
-        print("test")
