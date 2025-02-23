ce34e5e68e5660ceb0e531f40e83d25fb9ec3606
petrocb <136578164+petrocb@users.noreply.github.com>
2025-02-23 16:15:01 +0000

    UI updates (#8)
    * new better ui
    
    * bug fixes

--- tkinterFreshStart/main.py
+++ tkinterFreshStart/main.py
@@ -84,8 +84,8 @@ class SwimmerDetectionApp:
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(img)
         imgtk = ImageTk.PhotoImage(image=img)
-        self.label.imgtk = imgtk
-        self.label.configure(image=imgtk)
+        self.video_label.imgtk = imgtk
+        self.video_label.configure(image=imgtk)
         # print("Updating frame")
         self.tracker.track(results)
         self.root.after(10, self.updateFrame)
