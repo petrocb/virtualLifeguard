2850a43d5164d34ca2ed579a4d1eff7e06577f46
petrocb <k2124745@kingston.ac.uk>
2025-02-23 13:25:13 +0000

    fixed velocity

--- tkinterFreshStart/alerts/velocity.py
+++ tkinterFreshStart/alerts/velocity.py
@@ -1,5 +1,5 @@
 from math import sqrt, pow
-from datetime import timedelta
+from datetime import datetime, timedelta
 class velocity:
     def __init__(self):
         pass
@@ -9,13 +9,14 @@ class velocity:
         frames -= 1
         alertLocations = []
         if locations:
-            for object in locations:
-                object['steps'] = list(reversed(object['steps']))
+            # for object in locations:
+            #     object['steps'] = list(reversed(object['steps']))
             for object in locations:
                 if len(object['steps']) > frames:
-                    distance = sqrt(pow(object['steps'][frames]['x'] - object['steps'][0]['x'], 2) + pow(object['steps'][frames]['y'] - object['steps'][0]['y'], 2))
-                    velocity = distance / (object['steps'][0]['time'] - object['steps'][frames]['time']).total_seconds()
-                    print("The velocity of object id: " + str(object['id']) + " is " + str(velocity))
+                    if (datetime.utcnow() - object['steps'][0]['time']).total_seconds() < 5:
+                        distance = sqrt(pow(object['steps'][frames]['x'] - object['steps'][0]['x'], 2) + pow(object['steps'][frames]['y'] - object['steps'][0]['y'], 2))
+                        velocity = distance / (object['steps'][0]['time'] - object['steps'][frames]['time']).total_seconds()
+                        print("The velocity of object id: " + str(object['id']) + " is " + str(velocity))
 
         return alertLocations
 
